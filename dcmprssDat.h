0821dd9f4d8b8c3cf95ce76d4f2b40b71299d33f
Ozanis <37501284+ozanis@users.noreply.github.com>
2018-12-18 23:21:33 +0200

    commit

--- dcmprssDat.h
+++ dcmprssDat.h
@@ -8,27 +8,31 @@
 using namespace std;
 class Gzip {
 public:
+    unsigned * len;
+    std::stringstream ss;
+
+    Gzip(){
+            this->len=nullptr;
+            this->ss << "0";
+    };
+
+    void data_get(){
+            ss << sysconf(_SC_NPROCESSORS_ONLN) << "/" << get_cpu() << "/" <<
+               *(process_mem_usage()) << "/" << *(process_mem_usage() + 1) << "/" <<
+               *(physical_mem_usage()) << "/" << *(physical_mem_usage() + 1);
+    }
+
+    ~Gzip()=default;
     string compress() {
         namespace bio = boost::iostreams;
-        std::stringstream ss;
-        ss << sysconf(_SC_NPROCESSORS_ONLN) << "/" << get_cpu() << "/" <<
-           *(process_mem_usage()) << "/" << *(process_mem_usage() + 1) << "/" <<
-           *(physical_mem_usage()) << "/" << *(physical_mem_usage() + 1);
-
-        std::stringstream compressed;
-        //std::stringstream origin(data);
-
 
         bio::filtering_streambuf<bio::input> out;
         out.push(bio::gzip_compressor(bio::gzip_params(bio::gzip::best_compression)));
         out.push(ss);
         bio::copy(out, ss);
 
-        char telemetry[sizeof(ss) / 8];
-        ss >> telemetry;
-        std::cout << telemetry << std::endl;
-
-        return compressed.str();
+        std::cout << ss.str() << std::endl;
+        return ss.str();
     }
 
 };
